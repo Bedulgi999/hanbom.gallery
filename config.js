0908b83151e00ae23bf414542c820a8514d1aed4
Bedulgi999 <bedulgi999999999@gmail.com>
2025-12-08 20:53:21 +0900

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js — Supabase 전역 설정
-
-const supabase = window.supabase.createClient(
-    "https://glmytzfqxdtlhmzbcsgd.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdsbXl0emZxeGR0bGhtemJjc2dkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0Mzc4MjIsImV4cCI6MjA4MDAxMzgyMn0.8f0rAcPMUvLtY5EM9HI9uNOOOs5SKGNdC7A3U29cjyo"
-);
