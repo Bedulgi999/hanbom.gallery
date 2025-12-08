a180aa1164c9ab3be5a26989a38c3254b0daefd4
Bedulgi999 <bedulgi999999999@gmail.com>
2025-12-08 19:47:48 +0900

    Add files via upload

--- notice_view.js
+++ notice_view.js
@@ -1,10 +1,5 @@
 // notice_view.js
 
-const supabase = window.supabase.createClient(
-  "https://glmytzfqxdtlhmzbcsgd.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdsbXl0emZxeGR0bGhtemJjc2dkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0Mzc4MjIsImV4cCI6MjA4MDAxMzgyMn0.8f0rAcPMUvLtY5EM9HI9uNOOOs5SKGNdC7A3U29cjyo"
-);
-
 const params = new URLSearchParams(location.search);
 const postId = params.get("id");
 const loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
